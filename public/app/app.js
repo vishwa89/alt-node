2324dec2519c1eca27aefd86692d273da7c25632
boringtaoxh <boringtaoxh@gmail.com>
2014-07-15 04:45:34 +0100

    brand structure

--- public/app/app.js
+++ public/app/app.js
@@ -5,7 +5,8 @@ angular.module('alt')
 	$locationProvider.html5Mode(true);
 	$routeProvider
 	.when('/', { templateUrl: '/pages/index'})
-	.when('/brand/products', { templateUrl: '/pages/brand-products' })
-	.when('/brand/trace', { templateUrl: '/pages/brand-products' })
+	.when('/brand/products-all', { templateUrl: '/pages/brand-products-all' })
+	.when('/brand/products-featured', { templateUrl: '/pages/brand-products-featured' })
+	.when('/brand/trace', { templateUrl: '/pages/brand-products-all' })
 	.when('/brand/chapter/:chapter', { templateUrl: '/pages/brand-chapter' });
 });
