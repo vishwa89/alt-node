6b191475b7d5f08cc17ca01d756441806b6d6b60
boringtaoxh <boringtaoxh@gmail.com>
2014-07-22 09:57:30 +0100

    follow

--- public/app/products.js
+++ public/app/products.js
@@ -14,10 +14,30 @@ alt
 })
 .directive('myRepeatDirective', function() {
   return function(scope, element, attrs) {
+    angular.element(element).css('color','blue');
     if (scope.$last){
-	  trigger.click(function(){
-	    console.log(this);
-	  });
+      $('.tag-trigger').hover(function(){
+      	console.log(this);
+      	//$(this).parent().find('.tag_info').fadeIn();
+      	$(this).parents('.tag').find('.tag_info').fadeIn(500, 'easeOutExpo');
+      }, function(){
+      	$('.tag_info').fadeOut();
+      })
     }
   };
+})
+.directive('follow', function() {
+  return function(scope, element, attrs) {
+    $(".follow").click( function(event){
+    event.preventDefault();
+    console.log(this);
+    $('#follow').stop().fadeIn(200);
+    $('#follow').addClass("popup");
+    return false;
+  });
+    $(".close").click(function(){
+      $('#follow').stop().fadeOut(200);
+    $('#follow').removeClass("popup");
+    })
+  };  
 });
